6b0e724f4659c4fcf21ada47acb0f744777cccc9
Gregory Dutton <greg.dutton@gmail.com>
2023-06-30 16:20:56 -0700

    Lab complete w bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-function Article({title, date, preview}) {
+function Article({title, date, preview, minutes}) {
+    let coffee = "";
+    let bento = ""
+    let readTime
 
+    if (minutes < 30) {
+        for (let i=1; i <= Math.ceil(minutes/5); i++) {
+            coffee = coffee + "☕️";
+        }
+        readTime = coffee + " " + minutes + " min read"
+    } else if (minutes > 30) {
+        for (let i=1; i <= Math.ceil(minutes/10); i++) {
+            bento = bento + "🍱";
+        }
+        readTime = bento + " " + minutes + " min read"
+    } else {
+        readTime = "oops"
+    }
     return(
             <article>
                 <h3>{title}</h3>
                 <small>
-                    {date ? date : "January 1, 1970"}
+                    {date ? date : "January 1, 1970"} - {readTime}
                 </small>
                 <p>{preview}</p>
             </article>
